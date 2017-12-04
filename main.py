322e4f3fe07a0b88bd90f9923963de289278eb54
Christof Hullaert <christof.hullaert@cover3d.com>
2017-12-04 22:40:57 +0100

    part 1

--- main.py
+++ main.py
@@ -2,10 +2,4 @@ from spirale import Spirale
 
 spirale=Spirale()
 
-for i in range(1, 35):
-    #print i, spirale.first(i)
-    #print i, spirale.get_position(i)
-    #print i, spirale.sector(i)
-    print i, spirale.get_position_2(i)
-
 print spirale.get_distance(347991)
